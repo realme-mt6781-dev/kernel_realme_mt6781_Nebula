2daf434579f159402d3cfe4246ee80808630a70b
Roman Gushchin <guro@fb.com>
2017-11-06 13:30:29 -0500

    BACKPORT: cgroup: export list of cgroups v2 features using sysfs
    The active development of cgroups v2 sometimes leads to a creation
    of interfaces, which are not turned on by default (to provide
    backward compatibility). It's handy to know from userspace, which
    cgroup v2 features are supported without calculating it based
    on the kernel version. So, let's export the list of such features
    using /sys/kernel/cgroup/features pseudo-file.
    
    The list is hardcoded and has to be extended when new functionality
    is added. Each feature is printed on a new line.
    
    Example:
      $ cat /sys/kernel/cgroup/features
      nsdelegate
    
    Signed-off-by: Roman Gushchin <guro@fb.com>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: kernel-team@fb.com
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Change-Id: I2baf0b7bcc27491568772defc43a06d0a5ed46bf
    (cherry picked from commit 5f2e673405b742be64e7c3604ed4ed3ac14f35ce)
    Bug: 154548692
    Signed-off-by: Marco Ballesio <balejs@google.com>
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Signed-off-by: prathamdubey2005 <134331217+prathamdubey2005@users.noreply.github.com>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -6526,6 +6526,7 @@ static struct kobj_attribute cgroup_features_attr = __ATTR_RO(features);
 
 static struct attribute *cgroup_sysfs_attrs[] = {
 	&cgroup_delegate_attr.attr,
+	&cgroup_features_attr.attr,
 	NULL,
 };
 
