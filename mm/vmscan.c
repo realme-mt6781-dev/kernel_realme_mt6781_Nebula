12a8b56fb839911639338ed020b1613822792f4c
dreamisbaka <jolinux.g@gmail.com>
2021-01-22 04:15:42 +0800

    mm: Set default swappiness to 160
    Signed-off-by: dreamisbaka <jolinux.g@gmail.com>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Signed-off-by: LibXZR <xzr467706992@163.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -186,7 +186,7 @@ struct scan_control {
 /*
  * From 0 .. 200.  Higher means more swappy.
  */
-int vm_swappiness = 60;
+int vm_swappiness = 160;
 
 #ifdef CONFIG_DYNAMIC_TUNNING_SWAPPINESS
 int vm_swappiness_threshold1 = 0;
