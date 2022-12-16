fe007ce2714152647d38ddca6e77d8d268881a29
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2022-12-16 23:53:42 +0530

    drivers: mediatek: Adjust the values for minimal battery consump… …tion
    Tweak some of these values and disable some of the options so that we can achieve greater battery backup from the kernel.
    
    Signed-off-by: prathamdubey2005 <134331217+prathamdubey2005@users.noreply.github.com>

--- drivers/gpu/mediatek/ged/src/ged_kpi.c
+++ drivers/gpu/mediatek/ged/src/ged_kpi.c
@@ -328,12 +328,12 @@ static unsigned int gx_gpu_freq_avg;
 #ifdef GED_KPI_CPU_BOOST
 static int boost_accum_cpu;
 /* for non-GED_KPI_MAX_FPS-FPS cases */
-static long target_t_cpu_remained = 16000000;
+static long target_t_cpu_remained = 18600000;
 /* static long target_t_cpu_remained_min = 8300000; */
 /* default 0.5 vsync period */
-static int cpu_boost_policy;
-static int boost_extra=-1;
-static int boost_amp;
+static int cpu_boost_policy=-1;
+static int boost_extra=1;
+static int boost_amp=-1;
 static int deboost_reduce;
 static int boost_upper_bound = 55;
 static void (*ged_kpi_cpu_boost_policy_fp)(struct GED_KPI_HEAD *psHead,
