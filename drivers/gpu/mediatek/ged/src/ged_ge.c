6beb2030d632ed9a48f8d25c2bf81ead7ab4049e
mtk15701 <chien-lun.hsu@mediatek.com>
2023-02-04 12:48:25 +0800

    [ALPS07839663] GPU: GED: refine parameter checking
    [Detail]
    - Refine parameter checking to avoid overflow.
    - Refine size checking to avoid reference NULL pointer.
    
    MTK-Commit-Id: e3b3795207e574cf2510b99306861e7259b7b9a8
    
    CR-Id: ALPS07839663
    Feature: GED driver
    Signed-off-by: mtk15701 <chien-lun.hsu@mediatek.com>
    Change-Id: I5f4b79bcc650b327645352162a3db624ea6ed071
    (cherry-pick from 579373d11e01c411eb9f3d41043b6025af898010)
    (cherry-pick from 8bbdfa81026e33dae98926e9c6ed5b74d5448989)

--- drivers/gpu/mediatek/ged/src/ged_ge.c
+++ drivers/gpu/mediatek/ged/src/ged_ge.c
@@ -154,7 +154,7 @@ int ged_ge_alloc(int region_num, uint32_t *region_sizes)
 		// check region_sizes parameter
 		if (region_sizes[i] <= 0 ||
 			region_sizes[i] > GE_MAX_REGION_SIZE) {
-			GED_PDEBUG("check size fail rgion_sizes[%d]:%u\n",
+			GED_PDEBUG("check size fail region_sizes[%d]:%u\n",
 				i, region_sizes[i]);
 			goto err_parameter;
 		}
