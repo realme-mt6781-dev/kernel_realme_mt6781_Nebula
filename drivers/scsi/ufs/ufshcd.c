baefe6639035db3693bea5afa53bf0f0abef0bf0
VoidDev-0 <dev.avax@protonmail.com>
2023-02-24 17:48:44 +0100

    drivers/scsi: ufshcd: Remove MTK logging patches
    Signed-off-by: VoidDev-0 <dev.avax@protonmail.com>
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -9324,9 +9324,6 @@ out:
 		ktime_to_us(ktime_sub(ktime_get(), start)),
 		hba->curr_dev_pwr_mode, hba->uic_link_state);
 
-	dev_info(hba->dev, "ss,ret %d,%d us\n", ret,
-		(int)ktime_to_us(ktime_sub(ktime_get(), start)));
-
 	if (!ret)
 		hba->is_sys_suspended = true;
 	return ret;
@@ -9361,9 +9358,6 @@ out:
 		ktime_to_us(ktime_sub(ktime_get(), start)),
 		hba->curr_dev_pwr_mode, hba->uic_link_state);
 
-	dev_info(hba->dev, "sr,ret %d,%d us\n", ret,
-		(int)ktime_to_us(ktime_sub(ktime_get(), start)));
-
 	if (!ret)
 		hba->is_sys_suspended = false;
 	return ret;
@@ -9394,9 +9388,6 @@ out:
 	trace_ufshcd_runtime_suspend(dev_name(hba->dev), ret,
 		ktime_to_us(ktime_sub(ktime_get(), start)),
 		hba->curr_dev_pwr_mode, hba->uic_link_state);
-
-	dev_info(hba->dev, "rs,ret %d,%d us\n", ret,
-		(int)ktime_to_us(ktime_sub(ktime_get(), start)));
 
 	return ret;
 }
