7ff7769726629d8fdaf3f18f0a4b5bda89babbc3
bengris32 <bengris32@protonmail.ch>
2023-05-19 10:45:13 +0100

    kernel: uid: Nuke `OPLUS_FEATURE_UID_PERF` support
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I2405c1a0c3479c1f49f0c9a4b71ec9eb8a636b04

--- kernel/fork.c
+++ kernel/fork.c
@@ -186,9 +186,6 @@ static inline void free_task_struct(struct task_struct *tsk)
 
 #ifndef CONFIG_ARCH_THREAD_STACK_ALLOCATOR
 
-#ifdef CONFIG_OPLUS_FEATURE_UID_PERF
-extern void uid_perf_work_add(struct task_struct *task, bool force);
-#endif
 
 /*
  * Allocate pages if THREAD_SIZE is >= PAGE_SIZE, otherwise use a
@@ -2235,16 +2232,6 @@ static __latent_entropy struct task_struct *copy_process(
 	}
 
 	init_task_pid_links(p);
-#ifdef CONFIG_OPLUS_FEATURE_UID_PERF
-	/* should init uid pevents before task added into any link */
-	memset(p->uid_pevents, 0, sizeof(struct perf_event *) * UID_PERF_EVENTS);
-	memset(p->uid_counts, 0, sizeof(long long) * UID_PERF_EVENTS);
-	memset(p->uid_prev_counts, 0, sizeof(long long) * UID_PERF_EVENTS);
-	memset(p->uid_leaving_counts, 0, sizeof(long long) * UID_PERF_EVENTS);
-	memset(p->uid_group, 0, sizeof(long long) * UID_GROUP_SIZE);
-	memset(p->uid_group_prev_counts, 0, sizeof(long long) * UID_GROUP_SIZE);
-	memset(p->uid_group_snapshot_prev_counts, 0, sizeof(long long) * UID_GROUP_SIZE);
-#endif
 
 	if (likely(p->pid)) {
 		ptrace_init_task(p, (clone_flags & CLONE_PTRACE) || trace);
@@ -2308,10 +2295,6 @@ static __latent_entropy struct task_struct *copy_process(
 
         copy_oom_score_adj(clone_flags, p);
 
-#ifdef CONFIG_OPLUS_FEATURE_UID_PERF
-	if (!IS_ERR(p))
-		uid_perf_work_add(p, false);
-#endif
 	return p;
 
 bad_fork_cancel_cgroup:
