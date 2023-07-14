ab807b04b081c0b4dbcf8ea2f72f383042593c80
LinkBoi00 <linkdevel@protonmail.com>
2023-07-14 13:35:41 +0300

    drivers: gpu: drm: mediatek: Tone down AAL debug and verbose logging
    Change-Id: I33c8bd3e456313acc336441cda8f1adf24ee89b0
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- drivers/gpu/drm/mediatek/mtk_disp_aal.c
+++ drivers/gpu/drm/mediatek/mtk_disp_aal.c
@@ -288,20 +288,20 @@ static int disp_aal_get_cust_led(void)
 	"mediatek,lcd-backlight");
 	if (!led_node) {
 		ret = -1;
-		pr_debug("Cannot find LED node from dts\n");
+		pr_err("Cannot find LED node from dts\n");
 	} else {
 		ret = of_property_read_u32(led_node, "led_mode", &led_mode);
 		if (!ret)
 			atomic_set(&g_led_mode, led_mode);
 		else
-			pr_debug("led dts can not get led mode data.\n");
+			pr_err("led dts can not get led mode data.\n");
 
 		ret = of_property_read_u32_array(led_node,
 	    "pwm_config", pwm_config, ARRAY_SIZE(pwm_config));
 	}
 
 	if (ret)
-		pr_debug("get pwm cust info fail\n");
+		pr_err("get pwm cust info fail\n");
 	pr_debug("%s mode=%u\n", __func__, atomic_read(&g_led_mode));
 
 	return ret;
@@ -1004,7 +1004,7 @@ void dump_hist(struct DISP_AAL_HIST *data)
 			data->dre_enable);
 }
 
-static bool debug_dump_aal_hist;
+static bool debug_dump_aal_hist = 0;
 int mtk_drm_ioctl_aal_get_hist(struct drm_device *dev, void *data,
 	struct drm_file *file_priv)
 {
@@ -1426,7 +1426,7 @@ void dump_param(const struct DISP_AAL_PARAM *param)
 		param->allowPartial, param->refreshLatency);
 }
 
-static bool debug_dump_input_param;
+static bool debug_dump_input_param = 0;
 int disp_aal_set_param(struct mtk_ddp_comp *comp, struct cmdq_pkt *handle,
 		struct DISP_AAL_PARAM *param)
 {
@@ -1610,7 +1610,7 @@ static void disp_aal_clear_irq(struct mtk_ddp_comp *comp,
 }
 
 static bool debug_skip_dre3_irq;
-static bool debug_dump_reg_irq;
+static bool debug_dump_reg_irq = 0;
 static int dump_blk_x = -1;
 static int dump_blk_y = -1;
 #if defined(CONFIG_MTK_DRE30_SUPPORT)
@@ -1752,7 +1752,7 @@ static bool disp_aal_read_dre3(struct mtk_ddp_comp *comp,
 		}
 	}
 	if (dump_start >= 0)
-		pr_debug("[DRE3][HIST][%d-%d] %08x %08x %08x %08x %08x %08x\n",
+		pr_err("[DRE3][HIST][%d-%d] %08x %08x %08x %08x %08x %08x\n",
 			dump_blk_x, dump_blk_y,
 			dump_table[0], dump_table[1], dump_table[2],
 			dump_table[3], dump_table[4], dump_table[5]);
