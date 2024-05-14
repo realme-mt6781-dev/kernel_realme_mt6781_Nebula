f5e80b74693be2fc44ed1bae5cdcade5d0d40189
Sultan Alsawaf <sultan@kerneltoast.com>
2024-05-14 21:48:47 +0300

    sched/cass: Honor uclamp even when no CPUs can satisfy the requirement When all CPUs available to a uclamp'd process are thermal throttled, it is possible for them to be throttled below the uclamp minimum requirement. In this case, CASS only considers uclamp when it compares relative utilization and nowhere else; i.e., CASS essentially ignores the most important aspect of uclamp.
    Fix it so that CASS tries to honor uclamp even when no CPUs available to a
    uclamp'd process are capable of fully meeting the uclamp minimum.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -166,13 +166,13 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 		struct rq *rq = cpu_rq(cpu);
 
 		/* Get the original, maximum _possible_ capacity of this CPU */
-		curr->cap_orig = arch_scale_cpu_capacity(cpu);
+		curr->cap_orig = arch_scale_cpu_capacity(NULL, cpu);
 
 		/* Get the _current_, throttled maximum capacity of this CPU */
 		curr->cap_max = curr->cap_orig - thermal_load_avg(rq);
 
-		/* Prefer the CPU that meets the uclamp minimum requirement */
-		if (curr->cap_max < uc_min && best->cap_max >= uc_min)
+		/* Prefer the CPU that more closely meets the uclamp minimum */
+		if (curr->cap_max < uc_min && curr->cap_max < best->cap_max)
 			continue;
 
 		/*
