94e583e4ed674882eb260f9ae931453e9ee73137
Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
2024-05-21 20:05:38 +0000

    ARM64: configs: Enable CASS

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -168,9 +168,6 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 		/* Get the original, maximum _possible_ capacity of this CPU */
 		curr->cap_orig = arch_scale_cpu_capacity(NULL, cpu);
 
-		/* Get the _current_, throttled maximum capacity of this CPU */
-		curr->cap_max = curr->cap_orig - thermal_load_avg(rq);
-
 		/* Prefer the CPU that more closely meets the uclamp minimum */
 		if (curr->cap_max < uc_min && curr->cap_max < best->cap_max)
 			continue;
