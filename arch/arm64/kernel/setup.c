mikeNG/android_kernel_oneplus_sm8350

arm64: kernel: Fix offline charging
Change-Id: I2bb42ca8a92131e79031e2fad0850676a1746377

arch/arm64/kernel/setup.c
@@ -290,6 +290,21 @@ u64 cpu_logical_map(int cpu)
 }
 EXPORT_SYMBOL_GPL(cpu_logical_map);
 
+static int __init androidboot_startupmode(char *p)
+{
+	char *offset_addr;
+
+	if (strcmp(p, "usb_charger"))
+		return 1;
+
+	if ((offset_addr =
+		     strstr(boot_command_line, "androidboot.mode=normal")))
+		memset(offset_addr, ' ', sizeof("androidboot.mode=normal"));
+
+	return 0;
+}
+early_param("androidboot.startupmode", androidboot_startupmode);
+
 void __init setup_arch(char **cmdline_p)
 {
 	init_mm.start_code = (unsigned long) _text;

realme-mt6781-dev/kernel_realme_mt6781_Nebula

arch/arm64/kernel/setup.c
@@ -297,6 +297,21 @@ arch_initcall(reserve_memblock_reserved_regions);
 
 u64 __cpu_logical_map[NR_CPUS] = { [0 ... NR_CPUS-1] = INVALID_HWID };
 
+static int __init androidboot_startupmode(char *p)
+{
+	char *offset_addr;
+
+	if (strcmp(p, "dc_charger"))
+		return 1;
+
+	if ((offset_addr =
+		     strstr(boot_command_line, "androidboot.mode=normal")))
+		memset(offset_addr, ' ', sizeof("androidboot.mode=normal"));
+
+	return 0;
+}
+early_param("androidboot.startupmode", androidboot_startupmode);
+
 void __init setup_arch(char **cmdline_p)
 {
 	init_mm.start_code = (unsigned long) _text;