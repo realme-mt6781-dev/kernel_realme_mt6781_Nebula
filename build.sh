cf296d7a0893128075f3933b3fffe0b62ce093ea
Hellin <113885617+helliini@users.noreply.github.com>
2024-07-31 07:31:19 +0000

    Build_sh: Import the build script for the Nebula Kernel

--- build.sh
+++ build.sh
@@ -1,48 +1,58 @@
-#!/bin/bash
-clear
-function compile() 
-{
-echo Nebula, CODENAMED - spaced
-echo
-source ~/.bashrc && source ~/.profile
-export LC_ALL=C && export USE_CCACHE=1
-ccache -M 100G >/dev/null
-TANGGAL=$(date +"%Y%m%d-%H")
-export ARCH=arm64
-export KBUILD_BUILD_HOST=Nebula
-export KBUILD_BUILD_USER="HELLINFIX"
+#!/bin/env bash
 
-clangbin=clang/bin/clang
-if ! [ -a $clangbin ]; then mkdir clang && cd clang
-sudo apt install -y libelf-dev libarchive-tools zstd flex bc ccache
-bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) -S
-bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) --patch=glibc
-ls
-cd ..
-fi
-read -p "Wanna do dirty build? (Y/N): " build_type
-if [[ $build_type == "N" || $build_type == "n" ]]; then
-echo Deleting out directory and doing clean Build
-rm -rf out && mkdir -p out
-fi
-if [[ $build_type == "Y" || $build_type == "y" ]]; then
-echo Warning :- Doing dirty build
-fi
-if ! [[ $build_type == "Y" || $build_type == "y" ]]; then
-if ! [[ $build_type == "N" || $build_type == "n" ]]; then
-echo Invalid Input , Read carefully before typing
-echo Trying to restart script
-. build.sh && exit
-fi
-fi
+# Define compile function
+function compile() {
+  # Load environment variables
+  source ~/.bashrc
+  source ~/.profile
 
-make O=out ARCH=arm64 spaced_defconfig
+  # Set environment variables
+  export LC_ALL=C
+  export USE_CCACHE=1
 
-PATH="${PWD}/clang/bin:${PATH}" \
-make -j$(nproc --all) O=out \
-                      CC="clang" \
-                      LLVM=1 \
-                       CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log 
+  TANGGAL=$(date +"%Y%m%d-%H")
+  export KBUILD_BUILD_HOST=Nebula
+  export KBUILD_BUILD_USER="HELLINFIX"
+
+  # Allocate 100GB of memory to ccache
+  ccache -M 100G
+
+  # Install Kernel Dependencies
+  sudo apt install -y libelf-dev libarchive-tools zstd flex bc ccache
+
+  # Download clang if not present
+  if [[ ! -d "clang" ]]; then mkdir clang && cd clang
+  bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) -S
+  bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) --patch=glibc
+  ls
+  cd ..
+  fi
+
+  # create output directory and do a clean or dirty build
+  read -p "Wanna do dirty build? (Y/N): " build_type
+  if [[ $build_type == "N" || $build_type == "n" ]]; then
+  echo Deleting out directory and doing clean Build
+  rm -rf out && mkdir -p out
+  fi
+  if [[ $build_type == "Y" || $build_type == "y" ]]; then
+  echo Warning :- Doing dirty build
+  fi
+  if ! [[ $build_type == "Y" || $build_type == "y" ]]; then
+  if ! [[ $build_type == "N" || $build_type == "n" ]]; then
+  echo Invalid Input , Read carefully before typing
+  echo Trying to restart script
+  . build.sh && exit
+  fi
+  fi
+
+  # Build the kernel
+  make -j$(nproc --all) O=out ARCH=arm64 spaced_defconfig
+
+  # Add clang bin directory to PATH
+  PATH="${PWD}/clang/bin:${PATH}"
+
+  # Build the kernel with clang and log output
+  make -j$(nproc --all) O=out CC="clang" LLVM=1 CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee build.log
 }
 
 function zupload()
@@ -59,10 +69,11 @@ cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
 zip -r9 Nebula-${TANGGAL}.zip *
 curl -sL https://git.io/file-transfer | sh
-./transfer fio Nebula-${TANGGAL}.zip
+./transfer Nebula-${TANGGAL}.zip
 cd ../
 fi
 }
 
+# Run functions
 compile
 zupload
