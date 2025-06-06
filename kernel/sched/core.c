fadcc7963495a2f222d298a22eb990b85f61f5a8
HELLINFIX <111268251+HELLINFIX@users.noreply.github.com>
2025-06-06 19:23:49 +0000

    Revert "kernel: irq: Switch to bi-cluster API for marking IRQ threads as critical"
    This reverts commit 2d11f3201f12baebe1f4673c7c19fb86af23a11c.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1957,7 +1957,8 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 #endif
 
 	/* Don't allow perf-critical threads to have non-perf affinities */
-	if ((p->flags & PF_PERF_CRITICAL) && new_mask != cpu_perf_mask)
+	if ((p->flags & PF_PERF_CRITICAL) && new_mask != cpu_perf_mask &&
+	    new_mask != cpu_prime_mask)
 		return -EINVAL;
 
 	rq = task_rq_lock(p, &rf);
